385a60a666643499678ab0c7492fb59d328ad761
Py528 <pranaav.shinde5280@gmail.com>
2022-08-14 08:57:14 +0530

    JavaScript Functions

--- javascript.js
+++ javascript.js
@@ -1,30 +1,19 @@
-let a = "Helu World";
-
-let name, admin;
-name = "Pranav";
-admin = name;
-
-let ourPlantName = "Earth";
-let currentUserName = "Pranav";
-
-let userName1 = prompt("Who's there?", "");
-
-if (userName1 === "Admin") {
-  let pass = prompt("Password?", "");
-
-  if (pass === "TheMaster") {
-    alert("Welcome!");
-  } else if (pass === "" || pass === null) {
-    alert("Canceled");
-  } else {
-    alert("Wrong password");
-  }
-} else if (userName1 === "" || userName1 === null) {
-  alert("Canceled");
-} else {
-  alert("I don't know you");
+let a = 1;
+console.log(greater());
+// console.log(y(2,1));
+function greater(){
+  (function (num1 , num2){
+    let add =addition(num1 , num2);
+    return add;
+  })
 }
-
-let officalName = prompt("What's the “official” name of JavaScript?");
-
-(officalName === "ECMAScript") ? alert("Right") : alert("You don't know? “ECMAScript”!");
+  // function y(num1 , num2){
+    //   let sub = substraction(num1 , num2);
+  //   return sub;
+  // }
+  // function addition(q,w){
+  //   return q+w;
+  // }
+  // function substraction(q,w){
+  //   return q-w;
+  // }
