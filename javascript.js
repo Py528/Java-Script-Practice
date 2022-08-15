0ee022e4058edfa0130255b80d445d62cbd511ef
Py528 <pranaav.shinde5280@gmail.com>
2022-08-15 21:35:02 +0530

    Java Script Callback Function

--- javascript.js
+++ javascript.js
@@ -8,3 +8,10 @@ const pranav= (a, b=12) =>{
     return a*b
 }
 console.log(pranav(12));
+
+const log= (a) => {console.log(a)}
+outer(function(){log("1")}())
+
+setTimeout(function(){log("3")},6000)
+
+function outer(){log("2")}
