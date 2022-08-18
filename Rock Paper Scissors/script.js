7de5110e85705275a2d1141053f1cbb2844741aa
Py528 <pranaav.shinde5280@gmail.com>
2022-08-18 21:43:31 +0530

    Implemented counter

--- Rock Paper Scissors/script.js
+++ Rock Paper Scissors/script.js
@@ -1,18 +1,22 @@
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+let playerCounter = 0;
+let computerCounter = 0;
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     console.log("its a draw");
   } else if (playerSelection === "paper" && computerSelection === "scissor") {
-    console.log("scissor wins");
+    return computerCounter++;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
-    console.log("paper wins");
+    return playerCounter++;
   } else if (playerSelection === "scissor" && computerSelection === "paper") {
-    console.log("scissor wins");
+    return playerCounter++;
   } else if (playerSelection === "scissor" && computerSelection === "rock") {
-    console.log("rock wins");
+    return computerCounter++;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
-    console.log("paper wins");
+    return computerCounter++;
   } else if (playerSelection === "rock" && computerSelection === "scissor") {
-    console.log("rock wins");
+    return playerCounter++;
   } else {
     console.log("why not");
   }
@@ -21,14 +25,11 @@ function getComputerChoice() {
   let ar = ["paper", "rock", "scissor"];
   return ar[Math.floor(Math.random() * 3)];
 }
-const playerSelection = "scissor";
-// const computerSelection = getComputerChoice();
-game();
-
 
 function game() {
   for (let i = 0; i < 5; i++) {
-    const computerSelection = getComputerChoice();
-    playRound(playerSelection, computerSelection);
+    let computerSelectioa = getComputerChoice();
+    playRound(playerSelection, computerSelectioa);
   }
 }
+game();
