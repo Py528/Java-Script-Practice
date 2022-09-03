bd5fcd42a2eb06b809e2eaefb7d02cf192aeb1e7
Py528 <pranaav.shinde5280@gmail.com>
2022-09-03 19:54:15 +0530

    Basic Random Program

--- Practice/script.js
+++ Practice/script.js
@@ -1,1 +1,22 @@
 console.log("Hello Node!")
+
+let log = (log) =>{
+    console.log(log)
+}
+
+let getInfo = {
+    name:"Pranav",
+    age:18,
+    standard:"TYCM A",
+    rollNo:50
+}
+let arr=["name",'age','standard','rollNo']
+function randomSelection(){
+    let a = Math.floor(Math.random() * arr.length)
+    return arr[a];
+}
+
+// getInfo[]
+// log(randomSelection())
+log(getInfo[randomSelection()])
+
